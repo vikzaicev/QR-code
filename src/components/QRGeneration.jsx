2fdda6df91e25e40953a3e2f0123cc85d8b3ec71
vikzaicev <vikzaicev@mail.ru>
2024-12-17 15:56:07 +0500

    2

--- src/components/QRGeneration.jsx
+++ src/components/QRGeneration.jsx
@@ -1,11 +1,25 @@
 import { QRCodeSVG } from 'qrcode.react';
+import "./QRGenerator.css"
+import { useState } from 'react';
 
 export const QRGeneration = () => {
+    const [value, setValue] = useState('Привет')
+    const [result, setResalt] = useState('')
+
+    const handleBtn = (e) => {
+        setResalt(value)
+        setValue('')
+    }
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+        setResalt('')
+    }
     return (
-        <div>
-            <QRCodeSVG value="https://reactjs.org/" />
-            <input type="text" />
-            <button type="button">Сгенерировать QR</button>
+        <div className='container'>
+            <input onChange={handleInput} value={value} type="text" />
+            <button onClick={handleBtn} type="button">Сгенерировать QR</button>
+            {result != '' && <QRCodeSVG className="qr" value={result} />}
         </div>
     )
 }
